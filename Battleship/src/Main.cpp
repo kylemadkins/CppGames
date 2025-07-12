a599ed03b61ce50c97593d6af0f4122c22f7b486
Kyle Adkins <kyle.m.adkins@gmail.com>
2025-07-12 16:29:06 -0400

    Scoped enums

--- Battleship/src/Main.cpp
+++ Battleship/src/Main.cpp
@@ -1,97 +1,37 @@
 #include <iostream>
 
-int Index(int* arr, int row, int col, int rowLen) {
-	return arr[row * rowLen + col];
-}
+enum class ShipType {
+	None,
+	AircraftCarrier,
+	Battleship,
+	Cruiser,
+	Destroyer,
+	Submarine
+};
 
-void Fill(int* arr, int size) {
-	for (int i = 0; i < size; i++) {
-		arr[i] = i + 1;
-		//std::cout << "arr[" << i << "] = " << arr[i] << std::endl;
-	}
-}
+enum class ShipOrientation {
+	Horizontal,
+	Vertical
+};
 
-void Resize(int** arr, int size) {
-	if (*arr != nullptr) delete[] *arr;
-	*arr = new int[size];
-}
+struct ShipPosition {
+	int row;
+	int col;
+};
 
-int Sum(int* arr, int size);
-float Average(int* arr, int size);
-template <typename T>
-T Output(int* arr, int size, T (*Fn)(int*, int));
+struct Ship {
+	ShipType type = ShipType::None;
+	ShipOrientation orientation;
+	ShipPosition pos;
+	int size;
+};
 
-typedef int (*ArithmeticFn)(int x, int y);
-int Add(int x, int y);
-int Sub(int x, int y);
-ArithmeticFn GetArithmeticFn(char op);
+enum class GuessType {
+	None,
+	Miss,
+	Hit
+};
 
 int main() {
-	int size = 10;
-	int* arr = new int[size];
-	Fill(arr, size);
-
-	//size = 20;
-	//Resize(&arr, size);
-	//Fill(arr, size);
-
-	//std::cout << "sum: " << Output(arr, size, Sum) << std::endl;
-	//std::cout << "average: " << Output(arr, size, Average) << std::endl;
-
-	int* arr2d = new int[size * size];
-	Fill(arr2d, size * size);
-	//std::cout << "row 8, col 4 = " << arr2d[84] << std::endl;
-
-	int x;
-	int y;
-	char op;
-	std::cout << "Enter first number: ";
-	std::cin >> x;
-	std::cout << "Enter second number: ";
-	std::cin >> y;
-	std::cout << "Enter operation (+, -): ";
-	std::cin >> op;
-
-	ArithmeticFn fn = GetArithmeticFn(op);
-	std::cout << "Result: " << fn(x, y) << std::endl;
-
-	delete[] arr;
-	delete[] arr2d;
-}
-
-int Sum(int* arr, int size) {
-	int result = 0;
-	for (int i = 0; i < size; i++) {
-		result += arr[i];
-	}
-	return result;
-}
-
-float Average(int* arr, int size) {
-	float sum = Sum(arr, size);
-	return sum / size;
-}
-
-template <typename T>
-T Output(int* arr, int size, T (*Fn)(int*, int)) {
-	return Fn(arr, size);
-}
-
-int Add(int x, int y) {
-	return x + y;
-}
-
-int Sub(int x, int y) {
-	return x - y;
-}
-
-ArithmeticFn GetArithmeticFn(char op) {
-	switch (op) {
-	case '+':
-		return Add;
-	case '-':
-		return Sub;
-	default:
-		break;
-	}
+	return 0;
 }
